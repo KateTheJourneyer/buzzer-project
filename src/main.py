54256cb827d6f8b053ec1f61cffb6e3e527c9a61
KateTheJourneyer <55278079+KateTheJourneyer@users.noreply.github.com>
2026-02-08 16:41:35 +0700

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import os, sys, signal, queueZ
+
+class SoundBoard:
+	@staticmethod
+	def main(args: list[str]) -> None:
+		# Do stuff here
+
+		return
+
+if __name__=="__main__":
+	SoundBoard.main(sys.argv[:1])
